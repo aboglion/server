cbdd1dbb6ce1a764e6d3c6ec9354e74d638924d1
UNS <aboglion@gmail.com>
2025-07-20 23:03:41 +0300

    Update Makefile and docker-compose.yml

--- TRADER/main.py
+++ TRADER/main.py
@@ -88,6 +88,11 @@ def live_data():
             progress = f"{(len(coin.med_price_history)/Config.HISTORY_LIMIT)*100:.4f}"
             result[coin.symbol]["signal"] = progress+ "%"
             result[coin.symbol]["position"] = "collecting data.."
+            result[coin.symbol]["pnl_pct"] = None
+            result[coin.symbol]["total_buy_trades"] = None 
+            result[coin.symbol]["total_sell_trades"] = None
+            result[coin.symbol]["total_profit"] = None
+            result[coin.symbol]["binance_price"] = None
 
     return (jsonify({"data": result, "cycle_interval": Config.CYCLE_INTERVAL}), 200)
 
