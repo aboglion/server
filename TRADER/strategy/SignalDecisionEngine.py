154132293e066982ebdff784c04d20af269086d4
UNS <aboglion@gmail.com>
2025-07-23 09:53:30 +0300

    Update Makefile and docker-compose.yml

--- TRADER/strategy/SignalDecisionEngine.py
+++ TRADER/strategy/SignalDecisionEngine.py
@@ -111,12 +111,11 @@ class SignalDecisionEngine:
 
         COLLECTING_Progress = "COLLECTING 0%"
         if len(self.coin.med_price_history) < Config.HISTORY_LIMIT:
-            stage = f"COLLECTING stage0 {int(len(self.coin.med_price_history)/Config.HISTORY_LIMIT)*100}%"
+            COLLECTING_Progress = f"COLLECTING stage0 {int(len(self.coin.med_price_history)/Config.HISTORY_LIMIT)*100}%"
         else:
             max_len_collected = max(Config.HISTORY_LIMIT, Config.VOLATILITY_WINDOW, Config.PRESSURE_WINDOW, Config.VOLUME_WINDOW)
             min_pv = min(len(self.recent_signals), len(self.recent_buy_pressure), len(self.recent_sell_pressure), len(self.recent_volumes))
-            stage=f"COLLECTING {int(min_pv/max_len_collected)*100:.3f}%"
-            COLLECTING_Progress = f"{stage} 0%"
+            COLLECTING_Progress=f"COLLECTING {int(min_pv/max_len_collected)*100:.3f}%"
 
     
         if (len(self.coin.med_price_history) < max(Config.VOLATILITY_WINDOW,Config.HISTORY_LIMIT) or
